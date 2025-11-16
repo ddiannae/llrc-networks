e8a1860031a2772cb48603e37bccfe899b501322
Diana Garcia <diana.gco@gmail.com>
2025-11-15 20:34:47 -0500

    updating axis title

--- workflow/scripts/intraInterPlot.R
+++ workflow/scripts/intraInterPlot.R
@@ -1,6 +1,6 @@
 ################################################################################
 ## This script plots the intra-chromosomal interaction fractions for cancer and
-## normal tissue across different Mutual Information (MI) thresholds. 
+## normal tissue across different Mutual Information (MI) thresholds.
 ##
 ## Inputs:
 ##   - Fraction tables for intra/inter interactions in bins (from intraInterCount.R)
@@ -30,23 +30,23 @@ substring(TISSUE, 1, 1) <- toupper(substring(TISSUE, 1, 1))
 mi_data <- lapply(files, function(file) {
   read_tsv(file)
 })
-mi_data <- bind_rows(mi_data) 
+mi_data <- bind_rows(mi_data)
 mi_data$cond <- factor(mi_data$cond, levels = c("normal", "cancer"), labels = c("Normal", "Cancer"))
 
 if(BINTYPE == "onek") {
   mi_data$bin <- mi_data$bin * 1000
-} 
+}
 
 colors <- c("#e3a098", "#a32e27")
 
 cat("Building plot\n")
 p <- ggplot(mi_data, aes(y = intra_fraction, x = bin, color=cond)) +
   geom_point(size = 6) +
-  scale_x_log10(name="Total Interactions", breaks = c(1e3,1e4,1e5,1e6,1e7)) +
+  scale_x_log10(name="log Top10 MI pairs", breaks = c(1e3,1e4,1e5,1e6,1e7)) +
   theme_base(base_size = 30) +
-  theme(legend.position = c(0.85, 0.8), axis.title.y = element_text(size=28), 
+  theme(legend.position = c(0.85, 0.8), axis.title.y = element_text(size=28),
         plot.background=element_blank()) +
-  scale_y_continuous(name = "Fraction of intra-chromosomal", breaks = c(0, 0.25, 0.5, 0.75, 1), 
+  scale_y_continuous(name = "Fraction of intra-chromosomal", breaks = c(0, 0.25, 0.5, 0.75, 1),
                      limits = c(0, 1)) +
   scale_color_manual(name="Condition", values = colors) +
   ggtitle(TISSUE)
@@ -58,4 +58,4 @@ if(BINTYPE == "log") {
 cat("Saving plot\n")
 png(snakemake@output[[1]], width = 1200, height = 600)
 print(p)
-dev.off()  
+dev.off()
